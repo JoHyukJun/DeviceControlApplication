818c8ef14309f29dc7c521a130ed97df8d9a9610
Jo Hyuk Jun <hyukzuny@gmail.com>
2018-11-25 23:16:16 +0900

    Concept Update

--- app/src/main/java/com/example/johyukjun/project/HomeActivity.java
+++ app/src/main/java/com/example/johyukjun/project/HomeActivity.java
@@ -1,16 +1,72 @@
 package com.example.johyukjun.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+    ArrayList<String> m_Device;
+    ArrayAdapter<String> m_Adapter;
+    ListView m_ListView;
+    Button m_BtnAdd, m_BtnRemove;
+    EditText m_SerialNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ListView listView = (ListView) findViewById(R.id.listDevice);
+        m_Device = new ArrayList<String>();
+        m_Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, m_Device);
+
+        m_ListView = (ListView) findViewById(R.id.listDevice);
+        m_ListView.setAdapter(m_Adapter);
+        m_ListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        m_BtnAdd = (Button) findViewById(R.id.btnAddDevice);
+        m_BtnRemove = (Button) findViewById(R.id.btnRemoveDevice);
+
+        m_SerialNumber = (EditText) findViewById(R.id.editSerialNumber);
+
+        m_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    public void mOnClick (View v) {
+
+        switch (v.getId()) {
+            case R.id.btnAddDevice:
+                String text = m_SerialNumber.getText().toString();
+                if (text.length() != 0) {
+                    m_Device.add(text);
+                    m_SerialNumber.setText("");
+                    m_Adapter.notifyDataSetChanged();
+                }
+                break;
+
+            case R.id.btnRemoveDevice:
+                int pos;
+                pos = m_ListView.getCheckedItemPosition();
+                if (pos != ListView.INVALID_POSITION) {
+                    m_Device.remove(pos);
+                    m_ListView.clearChoices();
+                    m_Adapter.notifyDataSetChanged();
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/johyukjun/project/MainActivity.java
+++ app/src/main/java/com/example/johyukjun/project/MainActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText m_Id, m_Password;
+    private Button m_BtnLogIn, m_BtnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_BtnLogIn = (Button) findViewById(R.id.btnLogIn);
+        m_BtnSignUp = (Button) findViewById(R.id.btnSignUpActivity);
+
+        m_Id = (EditText) findViewById(R.id.editId);
+        m_Password = (EditText) findViewById(R.id.editPassword);
     }
 
     public void mOnClick(View v) {
@@ -18,8 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         switch (v.getId()) {
             case R.id.btnLogIn:
-                intent = new Intent(this, HomeActivity.class);
-                startActivity(intent);
+                String id = m_Id.getText().toString();
+                String pw = m_Password.getText().toString();
+
+                if (id.equals("jo") && pw.equals("0000")) {
+                    intent = new Intent(this, HomeManagerActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    intent = new Intent(this, HomeActivity.class);
+                    startActivity(intent);
+                }
                 break;
 
             case R.id.btnSignUpActivity:
