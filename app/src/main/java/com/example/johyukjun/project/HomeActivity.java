15c8ba8b498886561e56d0a0cbcb492173d1cb6d
Jo Hyuk Jun <hyukzuny@gmail.com>
2018-12-09 19:08:44 +0900

    CLASS MINOR UPDATE

--- app/src/main/java/com/example/johyukjun/project/HomeActivity.java
+++ app/src/main/java/com/example/johyukjun/project/HomeActivity.java
@@ -10,12 +10,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
-    ArrayList<String> m_Device;
-    ArrayAdapter<String> m_Adapter;
+    ArrayList<deviceItem> m_Device;
+    ArrayAdapter<deviceItem> m_Adapter;
     ListView m_ListView;
     Button m_BtnAdd, m_BtnRemove;
     EditText m_SerialNumber;
@@ -25,8 +26,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        m_Device = new ArrayList<String>();
-        m_Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, m_Device);
+        m_Device = new ArrayList<deviceItem>();
+        m_Adapter = new ArrayAdapter<deviceItem>(this, android.R.layout.simple_list_item_single_choice, m_Device);
 
         m_ListView = (ListView) findViewById(R.id.listDevice);
         m_ListView.setAdapter(m_Adapter);
@@ -47,12 +48,13 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void mOnClick (View v) {
+        Intent intent;
 
         switch (v.getId()) {
             case R.id.btnAddDevice:
                 String text = m_SerialNumber.getText().toString();
                 if (text.length() != 0) {
-                    m_Device.add(text);
+                    m_Device.add(new deviceItem(text, ""));
                     m_SerialNumber.setText("");
                     m_Adapter.notifyDataSetChanged();
                 }
@@ -67,6 +69,50 @@ public class HomeActivity extends AppCompatActivity {
                     m_Adapter.notifyDataSetChanged();
                 }
                 break;
+            case R.id.btnSelectDevice:
+                int selPos;
+                selPos = m_ListView.getCheckedItemPosition();
+                if (selPos != ListView.INVALID_POSITION) {
+                    intent = new Intent(this, Deivce_Activity.class);
+                    startActivity(intent);
+                }
+                break;
         }
+    }
+
+}
+
+
+class deviceItem {
+    private String serialNum;
+    private String nickName;
+
+    public deviceItem() {
+
+    }
+
+    public deviceItem(String inSerialNum, String inNickName) {
+        this.serialNum = inSerialNum;
+        this.nickName = inNickName;
+    }
+
+    public String GetSerialNum() {
+        return serialNum;
+    }
+
+    public String GetNickName() {
+        return nickName;
+    }
+
+    public void SetSerialNum(String inSerialNum) {
+        serialNum = inSerialNum;
+
+        return;
+    }
+
+    public void SetNickName(String inNickName) {
+        nickName = inNickName;
+
+        return;
     }
 }
